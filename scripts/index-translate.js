77a85824206aab615384efdbca5a9028683d7dc0
kimtaizigvy <65520928+kimtaizigvy@users.noreply.github.com>
2023-10-05 18:05:26 +0700

    chore: adjust data and control category translate key

--- scripts/index-translate.js
+++ scripts/index-translate.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable global-require */
 import {indexLanguage} from '../src/supported-locales';
 import * as fs from 'fs';
@@ -11,6 +12,25 @@ const baseUrl = `${process.env.METEOR_DOMAIN}/api`;
 let blockTransList = ['extensions', 'blocks'];
 let result = {};
 
+let blockIdTransform = id => {
+    // Normalize id to have _ format
+    const newId = id.replace('.', '_');
+    // eslint-disable-next-line no-unused-vars
+    const splitBlockName = newId.split('_');
+
+    // Checking if blockId is camelCase
+    // For handle some special case like: `data_setRandomList` block
+    if (splitBlockName.length === 2){
+        const [category, blockId] = splitBlockName;
+        const isCamel = /^([a-z]+)(([A-Z]([a-z]+))+)$/g.test(blockId);
+        if (isCamel) {
+            return `${category.toLowerCase()}_${blockId}`;
+        }
+    }
+
+    return newId.toLowerCase();
+};
+
 blockTransList.forEach((component) => {
     Object.keys(indexLanguage).forEach((lang) => {
         try {
@@ -23,24 +43,28 @@ blockTransList.forEach((component) => {
                     // Regex for remove %n and [] barack in translate string
                     const regex = /(?:%)\d+|\x5b|\x5D/g;
                     let newTranslate = translated.replace(regex, '');
-                    let newId = id.toLowerCase();
+
+                    // If camel case return original id
+                    let newId = blockIdTransform(id);
+                    
 
                     // For convert extension key to have same format with block type --> <category>_<block_id>
                     newId = newId.replace('.', '_');
                     // For getting category name ---> for later use (if have)
-                    const [category, blockName] = newId.split('_');
+                    const [category] = newId.split('_');
                     // For convert AI category blocks key to have same format with block type ---> <block_id>
                     newId = newId.replace('ai_', '');
                     // For convert operator blocks key to have same format with block type ---> <operator>_<block_id>
                     newId = newId.replace('operators_', 'operator_');
                     // For convert forever block to have same format with block type ---> forever
                     newId = newId.replace('control_forever', 'forever');
-                    // For convert my blocks category return block to have same format with block type ---> procedure_return
+                    // For convert my blocks category return block to have same format with block type
+                    // ---> procedure_return
                     newId = newId.replace('procedure_', 'procedures_');
 
                     set(result, [newId, lang], newTranslate);
 
-                    if (category) set(result, [newId, 'category'], category);
+                    if (category) set(result, [newId, 'category'], category.toLowerCase());
 
                     if (lang === 'zh-cn' && newTranslate) {
                         const convertedPinyin = pinyin(newTranslate, {toneToNumber: true}).replace(/[0-9]/g, '');
